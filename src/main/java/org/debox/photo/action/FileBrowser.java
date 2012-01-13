47d6ab22bac7d16e65312658987cd09cf4813e3a
Corentin Guy <corentin.guy@debox.fr>
2012-01-13 13:47:33 +0000

    Clean

--- src/main/java/org/debox/photo/action/FileBrowser.java
+++ src/main/java/org/debox/photo/action/FileBrowser.java
@@ -20,13 +20,9 @@
  */
 package org.debox.photo.action;
 
-import com.thebuzzmedia.exiftool.ExifTool;
-import com.thebuzzmedia.exiftool.ExifTool.Tag;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
 import javax.imageio.ImageIO;
 import org.debux.webmotion.server.WebMotionController;
 import org.debux.webmotion.server.render.Render;
@@ -37,48 +33,13 @@ import org.imgscalr.Scalr;
  */
 public class FileBrowser extends WebMotionController {
 
-    protected static final File source = new File("/home/cguy/public");
-    protected static final File dest = new File("/home/cguy/public_app");
+    protected static final File source = new File("/home/cguy/photos");
+    protected static final File dest = new File("/home/cguy/photos_thumbnails");
 
     public Render init() throws IOException {
         copyDirectories(source, dest);
-
         return renderStatus(200);
     }
-    
-    private static final int NONE = 0;
-private static final int HORIZONTAL = 1;
-private static final int VERTICAL = 2;
-private static final int[][] OPERATIONS = new int[][] {
-        new int[] {  0, NONE},
-        new int[] {  0, HORIZONTAL},
-        new int[] {180, NONE},
-        new int[] {  0, VERTICAL},
-        new int[] { 90, HORIZONTAL},
-        new int[] { 90, NONE},
-        new int[] {-90, HORIZONTAL},
-        new int[] {-90, NONE},
-        };
-
-public static BufferedImage rotateByExif(BufferedImage image) {
-//    try {
-//        int index = Integer.parseInt(image.getImageAttribute("EXIF:Orientation")) - 1;
-//        int degrees = OPERATIONS[index][0];
-//        if (degrees != 0)
-//            image = image.rotateImage(degrees);
-//        switch (OPERATIONS[index][1]) {
-//            case HORIZONTAL:
-//                image = image.flopImage();
-//                break;
-//            case VERTICAL:
-//                image = image.flipImage();
-//        }
-//    }
-//    catch (NumberFormatException exc) {}
-//    catch (NullPointerException exc) {}
-//    return image;
-    return null;
-}
     
     protected void copyDirectories(File source, File destParent) throws IOException {
         if (source.isDirectory()) {
@@ -92,13 +53,7 @@ public static BufferedImage rotateByExif(BufferedImage image) {
         } else {
             System.out.println(">>>" + source.getAbsolutePath());
             BufferedImage image = ImageIO.read(source);
-            ExifTool exifTool = new ExifTool();
-            Map<Tag, String> imageMeta = exifTool.getImageMeta(source, Tag.ORIENTATION);
-            
-            System.out.println(Arrays.toString(imageMeta.values().toArray()));
-            
-//            BufferedImage thumbnail = Scalr.resize(image, Scalr.Method.SPEED, Scalr.Mode.AUTOMATIC, 150, Scalr.OP_ANTIALIAS);
-            BufferedImage thumbnail = Scalr.resize(image, 150, 150);
+            BufferedImage thumbnail = Scalr.resize(image, 150);
             File imageFile = new File(destParent, source.getName());
             ImageIO.write(thumbnail, "jpg", imageFile);
         }
