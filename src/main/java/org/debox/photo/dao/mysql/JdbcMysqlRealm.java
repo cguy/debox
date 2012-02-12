8bc3c969cfe38869610eedeb5eef7ab6c937e98d
Corentin Guy <corentin.guy@debox.fr>
2012-02-12 15:26:53 +0000

    Export de la conf MySQL dans un fichier de configuration (et définition via properties Maven)

--- src/main/java/org/debox/photo/dao/mysql/JdbcMysqlRealm.java
+++ src/main/java/org/debox/photo/dao/mysql/JdbcMysqlRealm.java
@@ -21,10 +21,13 @@
 package org.debox.photo.dao.mysql;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.beans.PropertyVetoException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.crypto.RandomNumberGenerator;
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
@@ -43,8 +46,13 @@ public class JdbcMysqlRealm extends JdbcRealm {
     private static final Logger logger = LoggerFactory.getLogger(JdbcMysqlRealm.class);
     
     protected static final RandomNumberGenerator GENERATOR = new SecureRandomNumberGenerator();
-    
     protected static final String DEFAULT_SALTED_AUTHENTICATION_QUERY = "select password, password_salt, id from users where username = ?";
+    protected static final String PROPERTY_DATABASE_HOST = "database.host";
+    protected static final String PROPERTY_DATABASE_PORT = "database.port";
+    protected static final String PROPERTY_DATABASE_NAME = "database.name";
+    protected static final String PROPERTY_DATABASE_USER = "database.user";
+    protected static final String PROPERTY_DATABASE_PASSWORD = "database.password";
+    protected static final String PROPERTY_DATABASE_OPTIONS = "database.options";
     
     protected static ComboPooledDataSource comboPooledDataSource;
 
@@ -57,12 +65,25 @@ public class JdbcMysqlRealm extends JdbcRealm {
     public static synchronized ComboPooledDataSource getDataSource() {
         if (comboPooledDataSource == null) {
             try {
+                Properties properties = new Properties();
+                properties.load(JdbcMysqlRealm.class.getResourceAsStream("application.properties"));
+                
                 comboPooledDataSource = new ComboPooledDataSource();
                 comboPooledDataSource.setDriverClass("com.mysql.jdbc.Driver");
-                comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/debox-photos?autoReconnect=true");
-                comboPooledDataSource.setUser("root");
-                comboPooledDataSource.setPassword("");
-            } catch (Exception ex) {
+                
+                String host = properties.getProperty(PROPERTY_DATABASE_HOST);
+                String port = properties.getProperty(PROPERTY_DATABASE_PORT);
+                String name = properties.getProperty(PROPERTY_DATABASE_NAME);
+                String user = properties.getProperty(PROPERTY_DATABASE_USER);
+                String password = properties.getProperty(PROPERTY_DATABASE_PASSWORD);
+                String options = properties.getProperty(PROPERTY_DATABASE_OPTIONS);
+                
+                String url = "jdbc:mysql://" + host + ':' + port + '/' + name + options;
+                comboPooledDataSource.setJdbcUrl(url);
+                comboPooledDataSource.setUser(user);
+                comboPooledDataSource.setPassword(password);
+                
+            } catch (IOException | PropertyVetoException ex) {
                 logger.error(ex.getMessage(), ex);
             }
         }
